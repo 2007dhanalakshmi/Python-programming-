c1082c9bffd5366a1adbdef89af06e50e1fd32d7
2007dhanalakshmi <dhanalakshmi08112007@gmail.com>
2026-07-13 22:47:50 +0530

    Update Program2.py

--- Program2.py
+++ Program2.py
@@ -15,4 +15,20 @@ for i in range(r - 1, 0, -1):
     for j in range(0, i):
         print("*", end=" ")
     print()
-  
+
+output:
+        *
+
+       **
+       ***      
+       ****
+       *****
+        ****
+        ***
+         **
+          *
+
+
+
+
+
